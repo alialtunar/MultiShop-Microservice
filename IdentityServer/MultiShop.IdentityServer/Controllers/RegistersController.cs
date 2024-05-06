58b444f3741fb6c26fe7e0400912be9da59f9396
alialtunar <altunarali284@gmail.com>
2024-05-06 19:55:26 +0300

    Identity Server Created

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                UserName = userRegisterDto.Username,
+                Surname = userRegisterDto.Surname,
+            };
+
+            var result = await _userManager.CreateAsync(values,userRegisterDto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı başarıyla oluşturuldu");
+            }
+            else
+            {
+                return BadRequest("Bir hata oluştu tekrar deneyiniz");
+            }
+            
+        }
+    }
+}
