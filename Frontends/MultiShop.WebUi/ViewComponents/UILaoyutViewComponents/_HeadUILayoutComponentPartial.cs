1a9ef0691a79af7df188898748769335cf97f2e7
alialtunar <altunarali284@gmail.com>
2024-05-10 21:48:55 +0300

    Frontend Preparing

---
+++ Frontends/MultiShop.WebUi/ViewComponents/UILaoyutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUi.ViewComponents.UILaoyutViewComponents
+{
+    public class _HeadUILayoutComponentPartial:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
